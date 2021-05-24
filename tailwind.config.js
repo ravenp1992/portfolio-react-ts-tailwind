0945dc69f0663d5d8a2617de5e3edc715ac4c95f
Raven Paragas <ravenparagas@Ravens-MacBook-Pro.local>
2021-05-25 06:19:56 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Opean Sans', 'sans-serif']
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
